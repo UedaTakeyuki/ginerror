07a2fc49322a21df4e91c108c6e9a9e9596332df
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2021-04-10 04:46:12 +0000

    first commit

---
+++ oknghandler.go
@@ -0,0 +1,36 @@
+package ginerror
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func okngErrorOutWithMessage(c *gin.Context, message string) {
+	c.JSON(500, gin.H{
+		"message": errors.New(message),
+	})
+	//	c.Abort()
+	return
+}
+
+func okngErrorOut(c *gin.Context, err error) {
+	c.JSON(500, gin.H{
+		"message": err,
+	})
+	//	c.Abort()
+	return
+}
+
+func okngNotOwnerReturn() (err error) {
+	err = errors.New("not owner")
+	return
+}
+
+func okngNotOwnerErrOut(c *gin.Context) {
+	c.JSON(500, gin.H{
+		"message": "not owner",
+	})
+	//	c.Abort()
+	return
+}
